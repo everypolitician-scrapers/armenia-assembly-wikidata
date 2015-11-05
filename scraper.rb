88160d0b7f7416b553f66fdb6571391ff6e935b6
Tony Bowden <tony@tmtm.com>
2015-11-05 16:01:19 +0000

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,10 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'wikidata/fetcher'
+
+WikiData::Category.new('Կատեգորիա:ՀՀ_ԱԺ_5-րդ_գումարման_պատգամավորներ', 'hy').wikidata_ids.each do |id|
+  data = WikiData::Fetcher.new(id: id).data('hy') or next
+  ScraperWiki.save_sqlite([:id], data)
+end
