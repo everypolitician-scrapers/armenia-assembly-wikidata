f8ccfcabd7535c1db95bdc71795e6222703dc463
Tony Bowden <tony@tmtm.com>
2016-02-24 06:51:16 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,14 +1,7 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
 
-WikiData::Category.new('Կատեգորիա:ՀՀ_ԱԺ_5-րդ_գումարման_պատգամավորներ', 'hy').wikidata_ids.each do |id|
-  data = WikiData::Fetcher.new(id: id).data('hy') or next
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-require 'rest-client'
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
-
+names = WikiData::Category.new('Կատեգորիա:ՀՀ_ԱԺ_5-րդ_գումարման_պատգամավորներ', 'hy').member_titles
+EveryPolitician::Wikidata.scrape_wikidata(names: { hy: names })
