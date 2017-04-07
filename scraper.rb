a2693c6f696d596272b8cebc61fac64b6ed2fa89
Tony Bowden <tony@tmtm.com>
2017-04-07 18:47:00 +0100

    Also include ex-members from the term
    These are in a separate category

--- scraper.rb
+++ scraper.rb
@@ -3,5 +3,6 @@
 
 require 'wikidata/fetcher'
 
-names = WikiData::Category.new('Կատեգորիա:ՀՀ_ԱԺ_5-րդ_գումարման_պատգամավորներ', 'hy').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { hy: names })
+members = WikiData::Category.new('Կատեգորիա:ՀՀ_ԱԺ_5-րդ_գումարման_պատգամավորներ', 'hy').member_titles
+ex_members = WikiData::Category.new('Կատեգորիա:ՀՀ ԱԺ 5-րդ գումարման ընթացքում լիազորությունները դադարեցրած պատգամավորներ', 'hy').member_titles
+EveryPolitician::Wikidata.scrape_wikidata(names: { hy: members | ex_members })
