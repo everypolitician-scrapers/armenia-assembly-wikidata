b48ea21c2bf6935a0ab46c60b3e129bcecb54a12
Tony Bowden <tony@tmtm.com>
2018-05-23 16:20:40 +0100

    Also fetch from Wikidata P39s

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,9 @@
 
 require 'wikidata/fetcher'
 
+query = 'SELECT DISTINCT ?item { ?item wdt:P39 wd:Q17277248 }'
+ids = EveryPolitician::Wikidata.sparql(query)
+
 members = WikiData::Category.new('Կատեգորիա:ՀՀ_ԱԺ_5-րդ_գումարման_պատգամավորներ', 'hy').member_titles
 ex_members = WikiData::Category.new('Կատեգորիա:ՀՀ ԱԺ 5-րդ գումարման ընթացքում լիազորությունները դադարեցրած պատգամավորներ', 'hy').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { hy: members | ex_members })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { hy: members | ex_members })
