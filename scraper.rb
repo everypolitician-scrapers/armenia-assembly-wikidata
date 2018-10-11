42f2437b198f50fe9e71e565ef21661b1009171b
Tony Bowden <tony@tmtm.com>
2018-10-11 08:22:57 +0100

    Also get hywiki members of 6th Convocation

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,8 @@ require 'wikidata/fetcher'
 query = 'SELECT DISTINCT ?item { ?item wdt:P39 wd:Q17277248 }'
 ids = EveryPolitician::Wikidata.sparql(query)
 
-members = WikiData::Category.new('Կատեգորիա:ՀՀ_ԱԺ_5-րդ_գումարման_պատգամավորներ', 'hy').member_titles
+members5 = WikiData::Category.new('Կատեգորիա:ՀՀ_ԱԺ_5-րդ_գումարման_պատգամավորներ', 'hy').member_titles
+members6 = WikiData::Category.new('Կատեգորիա:ՀՀ_ԱԺ_6-րդ_գումարման_պատգամավորներ', 'hy').member_titles
 ex_members = WikiData::Category.new('Կատեգորիա:ՀՀ ԱԺ 5-րդ գումարման ընթացքում լիազորությունները դադարեցրած պատգամավորներ', 'hy').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { hy: members | ex_members })
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { hy: members5 | members6 | ex_members })
