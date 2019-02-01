7f3b17b4f1b94caa509923aae86862c46bd96826
Tony Bowden <tony@tmtm.com>
2019-02-01 15:45:33 +0000

    Add members of 7th Convocation

--- scraper.rb
+++ scraper.rb
@@ -11,6 +11,7 @@ has_parlid_ids = EveryPolitician::Wikidata.sparql(has_parlid_spq)
 
 members5 = WikiData::Category.new('Կատեգորիա:ՀՀ_ԱԺ_5-րդ_գումարման_պատգամավորներ', 'hy').member_titles
 members6 = WikiData::Category.new('Կատեգորիա:ՀՀ_ԱԺ_6-րդ_գումարման_պատգամավորներ', 'hy').member_titles
+members7 = WikiData::Category.new('Կատեգորիա:ՀՀ_ԱԺ_7-րդ_գումարման_պատգամավորներ', 'hy').member_titles
 ex_members = WikiData::Category.new('Կատեգորիա:ՀՀ ԱԺ 5-րդ գումարման ընթացքում լիազորությունները դադարեցրած պատգամավորներ', 'hy').member_titles
 
 EveryPolitician::Wikidata.scrape_wikidata(ids: has_p39_ids | has_parlid_ids, names: { hy: members5 | members6 | ex_members })
